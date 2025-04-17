d5d3005391236e043b4cc47267b8b3a633327f0c
luistarang0 <l22280377@toluca.tecnm.mx>
2025-04-16 19:46:35 -0600

    Implementacion Logica Encuesta de Satisfacción

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,5 +18,29 @@ public class HomeController : Controller
         return View();
     }
 
-    
+    [HttpPost]
+    public IActionResult CalcularSatisfaccion([FromBody] EncuestaSatisfaccion datos)
+    {
+        var respuestas = new List<int> { datos.Platform, datos.Test, datos.Minigame };
+
+        double promedio = respuestas.Average(); // PS: 1 a 5
+        double porcentaje = ((promedio - 1) / 4) * 100; // Normalización 0–100
+
+        string interpretacion;
+        if (porcentaje <= 20) interpretacion = "Muy insatisfecho";
+        else if (porcentaje <= 40) interpretacion = "Insatisfecho";
+        else if (porcentaje <= 60) interpretacion = "Neutral";
+        else if (porcentaje <= 80) interpretacion = "Satisfecho";
+        else interpretacion = "Muy satisfecho";
+
+        // Aquí podrías guardar en la base de datos también...
+
+        return Json(new
+        {
+            porcentaje = Math.Round(porcentaje, 2),
+            interpretacion,
+            comentario = datos.Comments
+        });
+    }
+
 }
---
+++ Models/EncuestaSatisfaccion.cs
@@ -0,0 +1,10 @@
+﻿namespace OK.Models
+{
+    public class EncuestaSatisfaccion
+    {
+        public int Platform { get; set; }
+        public int Test { get; set; }
+        public int Minigame { get; set; }
+        public string Comments { get; set; }
+    }
+}
